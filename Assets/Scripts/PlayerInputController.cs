8c91ff40ef28b82915287cc3740a8059a9ad1394
ShinHyeonSoo <106219658+ShinHyeonSoo@users.noreply.github.com>
2024-10-07 12:24:41 +0900

    Add PlayerController
    Add PlayerInputController,
    Add TopDownController,
    Add TopDownMovement

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : TopDownController
+{
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+        // 실제 움직이는 처리는 여기서하는게 아니라 PlayerMovement에서 함
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
+        newAim = (worldPos - (Vector2)transform.position).normalized;
+
+        CallLookEvent(newAim);
+    }
+}
---
+++ Assets/Scripts/TopDownMovement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    // 실제로 이동이 일어날 컴포넌트
+    private TopDownController _controller;
+    private Rigidbody2D _movementRigidbody;
+
+    private Vector2 _movementDirection = Vector2.zero;
+
+    private void Awake()
+    {
+        // 주로 내 컴포넌트 안에서 끝나는 것
+        _controller = GetComponent<TopDownController>();
+        _movementRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        _controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        _movementDirection = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        // FixedUpdate는 물리업데이트 관련
+        // rigidbody의 값을 바꾸니까 FixedUpdate
+        ApplyMovement(_movementDirection);
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * 5;
+        _movementRigidbody.velocity = direction;
+    }
+}
