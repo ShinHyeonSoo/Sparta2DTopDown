7539f2de44a69560e181863471a224efffd1dfbc
ShinHyeonSoo <106219658+ShinHyeonSoo@users.noreply.github.com>
2024-10-07 15:06:37 +0900

    Add Shooting
    클릭 시 화살 생성

--- Assets/Scripts/TopDownController.cs
+++ Assets/Scripts/TopDownController.cs
@@ -7,6 +7,30 @@ public class TopDownController : MonoBehaviour
 {
     public event Action<Vector2> OnMoveEvent; // Action은 무조건 void만 반환, 아니면 Func
     public event Action<Vector2> OnLookEvent;
+    public event Action OnAttackEvent;
+
+    protected bool _isAttacking { get; set; }
+
+    private float _timeSinceLastAttack = float.MaxValue;
+
+    private void Update()
+    {
+        HandleAttackDelay();
+    }
+
+    private void HandleAttackDelay()
+    {
+        // TODO : 매직 넘버 수정
+        if(_timeSinceLastAttack < 0.2f)
+        {
+            _timeSinceLastAttack += Time.deltaTime;
+        }
+        else if(_isAttacking && _timeSinceLastAttack >= 0.2f)
+        {
+            _timeSinceLastAttack = 0f;
+            CallAttackEvent();
+        }
+    }
 
     public void CallMoveEvent(Vector2 direction)
     {
@@ -16,5 +40,10 @@ public class TopDownController : MonoBehaviour
     public void CallLookEvent(Vector2 direction)
     {
         OnLookEvent?.Invoke(direction);
+    }
+
+    public void CallAttackEvent()
+    {
+        OnAttackEvent?.Invoke();
     }
 }
---
+++ Assets/Scripts/TopDownShooting.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+public class TopDownShooting : MonoBehaviour
+{
+    private TopDownController _controller;
+
+    [SerializeField]
+    private Transform _projectileSpawnPos;
+    private Vector2 _aimDirection = Vector2.right;
+
+    public GameObject testPrefab;
+
+    private void Awake()
+    {
+        _controller = GetComponent<TopDownController>();
+    }
+
+    private void Start()
+    {
+        _controller.OnAttackEvent += OnShoot;
+
+        _controller.OnLookEvent += OnAim;
+    }
+
+    private void OnAim(Vector2 direction)
+    {
+        _aimDirection = direction;
+    }
+
+    private void OnShoot()
+    {
+        CreateProjectile();
+    }
+
+    private void CreateProjectile()
+    {
+        // TODO : 날아가지 않기 때문에 날아가게 만들 것
+        Instantiate(testPrefab, _projectileSpawnPos.position, Quaternion.identity);
+    }
+}
